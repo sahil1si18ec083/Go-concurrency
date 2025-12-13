18540ce32632a1769fcaf42687b58d0e96d4b3ca
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-12-14 04:22:07 +0530

    Update channel_basic.go

--- 01_channels/channel_basic.go
+++ 01_channels/channel_basic.go
@@ -12,3 +12,26 @@ func main() {
 	x := <-ch
 	fmt.Println(x)
 }
+
+// package main
+// import "fmt"
+
+// func ping (s string ,  pingChan chan string){
+       
+//   pingChan <-s
+// }
+// func pong (pingChan chan string, pongChan chan string){
+       
+//       val := <-pingChan
+//      pongChan <-val
+// }
+// func main() {
+  
+//      pingChan := make(chan string)
+// 	   pongChan := make(chan string)
+	
+// 	   go ping ("hello", pingChan)
+// 	   go pong (pingChan, pongChan)
+// 	   fmt.Println(<-pongChan)
+	
+// }
