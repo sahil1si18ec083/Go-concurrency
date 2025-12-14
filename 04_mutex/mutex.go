e32240eed930c6dc1247ae3858acd0629cded876
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2025-12-14 05:50:22 +0530

    Create mutex.go

---
+++ 04_mutex/mutex.go
@@ -0,0 +1,29 @@
+
+package main
+import "fmt"
+import "sync"
+type Counter struct{
+    v map[string]int
+}
+ func (c *Counter) inc(key string, mu *sync.Mutex, wg *sync.WaitGroup){
+    defer wg.Done()
+    mu.Lock()
+    defer mu.Unlock()
+    c.v[key]++
+    
+}
+
+func main() {
+  var mu sync.Mutex
+  var wg sync.WaitGroup
+  fmt.Println("Try programiz.pro")
+  c:= Counter {v : make(map[string]int)}
+  for i:=0;i<10000;i++{
+      wg.Add(1)
+     go  c.inc("some key", &mu, &wg)
+  }
+  wg.Wait()
+  
+  
+  fmt.Println(c)
+}
